7c78c68b09ba47c40b640badeca64d317f2db2d1
 Apple iMac <iMac@iMac.local>
2019-07-11 16:55:03 +0300

    add some animation to states

--- src/components/Input.js
+++ src/components/Input.js
@@ -6,7 +6,7 @@ import {Attach, Send} from '../assets/assets';
 export const Input = ({value}) => {
   return (
     <div class="gs-chat__input">
-      <textarea value={value} placeholder="Ваше сообщение">{value}</textarea>
+      <textarea value={value} placeholder="Ваше сообщение" rows="1">{value}</textarea>
       <div class="gs-chat__input__icons">
         <button className="gs-chat__input__attach"><Attach/></button>
         <button className="gs-chat__input__send">{value ? <SentButton position="absolute" parent="input" size="min"/> :
